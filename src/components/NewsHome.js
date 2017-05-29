3084750095082c23ee11abd894463a53c89e5571
Vanessa Bell <vanessa.bell14@gmail.com>
2017-05-28 19:57:06 -0700

    axios request for nyt articles and formatted

--- src/components/NewsHome.js
+++ src/components/NewsHome.js
@@ -1,6 +1,34 @@
 import React, {Component} from 'react'
+import axios from 'axios';
+import moment from 'moment';
 
 class NewsHome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    const APIKey = '2eca7df7c2204c69b0f72c2c7f62afc3';
+    let term = "abortion"
+    var _this = this;
+    return axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+      params: {
+          'api-key': APIKey,
+          'q': term,
+      }
+    })
+    .then(function(results){
+          console.log(results.data.response.docs)
+          _this.setState({
+            articles: results.data.response.docs
+          });
+        })
+  }
+  
   render() {
     return (
      <section id="recent-news" name="recent-news">
@@ -14,7 +42,29 @@ class NewsHome extends Component {
           <h3 className="first-line pull-right">catch up on the latest news on abortion</h3>
         </div>
       </div>
-      <div className="scrollable">
+      <div className="row">
+        <div className="col-lg-11 col-md-11 col-sm-11 col-xs-11">
+          <ul className="news-articles text-right scrollable">
+            {this.state.articles.map(article =>
+              <li key={article._id}>
+                <div className="row">
+                  <div className="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+                    <h3>{article.headline.main}</h3>
+                    <p>{moment(article.pub_date).format('MMMM Do, YYYY')}</p>
+                    <p className="snippet">{article.snippet}</p>
+                    <p><a href={article.web_url} target="_blank" rel="noopener noreferrer">read more ></a></p>
+                  </div>
+                  <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                      {
+                        <img className="thumbnail" src={article.multimedia.length ? 'http://www.nytimes.com/' + article.multimedia[0].url : 'http://www.sgberman.com/wp-content/uploads/2014/10/nyt.png'} alt="article thumbnail"/>
+                      }
+                  </div>
+                </div>
+                
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
      </section>
     )
@@ -22,4 +72,3 @@ class NewsHome extends Component {
 }
 
 export default NewsHome;
-
---
+++ src/utils/helpers.js
@@ -0,0 +1,66 @@
+// TODO rewrite this
+
+// var axios = require('axios');
+
+// var APIKey = 'af40e2fed9c948f5a699bd16dfe80863';
+
+// var helpers = {
+
+//   runQuery: function(term, start, end)  {
+
+//     var term = term.trim();
+//     var start = start.trim() + "0101";
+//     var end = end.trim() + "1231";
+
+//     return axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+//       params: {
+//           'api-key': APIKey,
+//           'q': term,
+//           'begin_date': start,
+//           'end_date': end
+//       }
+//     })
+
+//     .then(function(results){
+
+//       return results.data.response;
+
+//     });
+//   },
+
+//   getSaved: function(){
+
+//     return axios.get('/api/saved')
+//       .then(function(results){
+
+//         return results;
+//       })
+//   },
+
+//   postSaved: function(title, date, url){
+
+//     var newArticle = {title: title, date: date, url: url};
+//     return axios.post('/api/saved', newArticle)
+//       .then(function(results){
+//         return results._id;
+//       })
+
+//   },
+
+//   deleteSaved: function(title, data, url){
+
+//     return axios.delete('/api/saved', {
+//       params: {
+//           'title': title,
+//           'data': data,
+//           'url': url,
+//       }
+//     })
+//     .then(function(results){
+//       return results;
+//     })
+//   }
+
+// }
+
+// module.exports = helpers;
